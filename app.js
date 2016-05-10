3ac356ef4aede29e89773f9990be611bcb8706e5
colon <colon.song@gmail.com>
2016-05-11 00:15:23 +0800

    test next function

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ app.get('/', function(req, res){
    res.send('Hello world ! '); 
 });
 
+app.get('/nextfunction', function (req, res, next) {
+  console.log('the response will be sent by the next function ...');
+  next();
+}, function (req, res) {
+  res.end();
+});
+
 app.listen(3000, function(){
    console.log('Example app listening on port 3000'); 
 });
